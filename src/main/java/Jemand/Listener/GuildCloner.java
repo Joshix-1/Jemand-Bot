0771e3ce913f1cb5a7aef2e84874fe0b57a73be6
Joshua <dingden04@gmail.com>
2020-08-04 17:09:20 +0200

    Move An events to GuildCloner and add some

--- src/main/java/Jemand/Listener/GuildCloner.java
+++ src/main/java/Jemand/Listener/GuildCloner.java
@@ -1,8 +1,16 @@
 package Jemand.Listener;
 
+import Jemand.func;
 import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.auditlog.AuditLogActionType;
 import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.permission.Permissions;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.event.channel.server.ServerChannelDeleteEvent;
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.event.server.member.ServerMemberBanEvent;
+import org.javacord.api.event.server.role.RoleChangePermissionsEvent;
 import org.javacord.api.event.server.role.UserRoleAddEvent;
 import org.javacord.api.util.logging.ExceptionLogger;
 
@@ -17,7 +25,18 @@ public class GuildCloner {
         api.getServerById(AN).ifPresent(server -> {
             server.addUserRoleAddListener(this::userAddedRole);
             server.addMessageCreateListener(this::messageCreated);
+            server.addServerMemberBanListener(this::memberBanned);
+            server.addServerChannelDeleteListener(this::channelDeleted);
+            server.addRoleChangePermissionsListener(this::roleChangedPermission);
         });
+    }
+
+    private void roleChangedPermission(RoleChangePermissionsEvent event) {
+        if (event.getRole().getName().equalsIgnoreCase("muted")) {
+            event.getRole().updatePermissions(Permissions.fromBitmask(0)).exceptionally(ExceptionLogger.get());
+        } else if (event.getRole().getId() != MITGLIED) {
+            event.getRole().updatePermissions(Permissions.fromBitmask(267902401)).exceptionally(ExceptionLogger.get());
+        }
     }
 
     private void userAddedRole(UserRoleAddEvent event) {
@@ -36,5 +55,58 @@ public class GuildCloner {
 
             }
         });
+
+        if(!event.getMessageAuthor().isRegularUser() || (event.getMessageContent().isEmpty() && event.getMessage().getEmbeds().isEmpty())) return;
+        User user = event.getMessageAuthor().asUser().orElse(null);
+        Server server = event.getServer().orElse(null);
+        if (user != null && server != null && user.getRoles(server).size() < 2) {
+            try {
+                user.addRole(server.getRoleById(559141475812769793L).orElseThrow(() -> new AssertionError("Rolle nicht da"))).join();
+                user.addRole(server.getRoleById(559444155726823484L).orElseThrow(() -> new AssertionError("Rolle nicht da"))).join();
+            } catch (Exception e) {
+                func.handle(e);
+            }
+        }
     }
+
+    private void memberBanned(ServerMemberBanEvent event) {
+        if (func.userIsTrusted(event.getUser())) {
+            event.getServer().unbanUser(event.getUser());
+        }
+    }
+
+    private void channelDeleted(ServerChannelDeleteEvent event) {
+        try {
+            event.getServer().getAuditLog(1, AuditLogActionType.CHANNEL_DELETE).join().getInvolvedUsers().forEach(user1 -> {
+                user1.removeRole(func.getApi().getRoleById(MITGLIED).orElseThrow(() -> new AssertionError("Mitgliedsrolle nicht da")), "Channel got deleted").join();
+                event.getServer().getOwner()
+                        .sendMessage(user1.getDisplayName(event.getServer()) + " (name: " + user1.getDiscriminatedName() + "; id: " + user1.getIdAsString() + ") hat #" + event.getChannel().getName() + " gelöscht.").join();
+
+            });
+        } catch (Exception e) {
+            func.handle(e);
+        }
+    }
+
+//Fortnite-Detektor:
+        //server.addUserChangeActivityListener(event -> {
+        //	server.getTextChannelById(623940807619248148L).ifPresent(textchannel -> {
+        //		event.getNewActivity().ifPresent(activity -> {
+        //			event.getOldActivity().ifPresent(oldactivity -> {
+        //				if (!oldactivity.getApplicationId().orElse(0L).equals(activity.getApplicationId().orElse(1L)) && ((activity.getType().equals(ActivityType.PLAYING) && activity.getName().equals("Fortnite")) || (activity.getApplicationId().orElse(0L) == 432980957394370572L))) {
+        //					EmbedBuilder embed = new EmbedBuilder()
+        //							.setColor(Color.RED)
+        //							.setTimestampToNow()
+        //							.setFooter(event.getUser().getDiscriminatedName(), event.getUser().getAvatar());
+        //					activity.getDetails().ifPresent(string -> {
+        //						embed.addField("\u200B", "\nDetails: (" + string + ")");
+        //					});
+        //					textchannel.sendMessage(embed.setDescription(event.getUser().getMentionTag() + " spielt " + activity.getName() + "."));
+        //					func.getApi().getRoleById(623193804551487488L).ifPresent(event.getUser()::addRole);
+        //				}
+        //			});
+        //		});
+        //	});
+        //	//}
+        //});
 }
